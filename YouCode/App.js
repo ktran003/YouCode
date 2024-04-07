053ddd18f75d5ca43437530da6f3503ea4e9934f
Kennedy Yung <97806971+kennedyyung@users.noreply.github.com>
2024-04-07 01:09:36 -0700

    test

--- YouCode/App.js
+++ YouCode/App.js
@@ -6,7 +6,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/loginScreen';
 import map from './src/screens/map';
 import BottomDrawer from './src/components/bottomDrawer';
-import discover from './src/screens/discover';
 import Discover from './src/screens/discover';
 
 const Stack = createNativeStackNavigator();
